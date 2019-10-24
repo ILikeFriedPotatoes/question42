9ab5f9d8abf141c6e772994e074bcb447f9f2a14
Joseph Wang <jwang@ucvts.tec.nj.us>
2019-10-24 09:18:19 -0400

    Saving work

--- question42/question42.java
+++ question42/question42.java
@@ -1,5 +1,14 @@
-public class() {
+public class question42 () {
 
-    //long originUnit = in.nextLong();
-    //printf("", )
+        System.out.print("Enter base: ");
+        long base = in.nextLong();
+        System.out.print("\nEnter height: ");
+        long height = in.nextLong();
+        System.out.print("\nEnter hypotenuse: ");
+        long hypotenuse = in.nextLong();
+        if((base * base) + (height * height) == (hypotenuse * hypotenuse)) {
+            System.out.print("\nYes, that's a right triangle!");
+        } else {
+            System.out.print("Nope...not a right triangle");
+        }
 }
